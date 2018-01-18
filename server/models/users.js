7ba0da31fb6c341dbafb1a6fcfecf5707901e707
Anri <anri.tora@gmail.com>
2018-01-18 13:20:23 -0800

    User Model and Migration File

---
+++ server/models/users.js
@@ -0,0 +1,17 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  var users = sequelize.define('users', {
+    firstName: DataTypes.STRING,
+    lastName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    displayName: DataTypes.STRING,
+    password: DataTypes.STRING
+  }, {
+    classMethods: {
+      associate: function(models) {
+        // associations can be defined here
+      }
+    }
+  });
+  return users;
+};
