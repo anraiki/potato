1af23cb7113b4d0df168b38c0639f8d421876029
Anri <anri.tora@gmail.com>
2018-01-18 20:07:59 -0800

    Updated dependency. Created Testing routes for registration endpoint and JWT features.

--- server/server.js
+++ server/server.js
@@ -1,10 +1,31 @@
-const express = require('express');
+const express     = require('express');
+const Sequelize   = require('sequelize');
+const bodyParser  = require("body-parser")
+
+const sequelize = new Sequelize("database","username","password", {
+  host: "localhost",
+  dialect: "sqlite",
+  storage: "database.sqlite"
+});
+
+sequelize.authenticate()
+  .then(() => {
+    console.log("Connection has been established through SQLITE")
+  })
+  .catch(err => {
+    console.log("An error has occured", err);
+  })
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.get('/api/hello', (req, res) => {
-  res.send({ express: 'Hello From Express' });
+//Body Parser for Express
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.post('/api/register' , (req, res) => {
+  let data = req.body;
+  console.log(data);
+  res.status(200).json({message: "Hello World"});
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
