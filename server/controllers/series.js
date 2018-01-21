a8c92db1f033cff4e07503758068fa309684b4ab
Anri <anri.tora@gmail.com>
2018-01-21 02:02:22 -0800

    Series Controller for retrieving a list of series and individually by their ID.

---
+++ server/controllers/series.js
@@ -0,0 +1,25 @@
+let models          = require("~/models"),
+    _               = require("lodash"),
+    validator       = require("validator");
+
+exports.get_list_of_series = async (req, res) => {
+
+    let series_db = models.series;
+    let results = await series_db.findAll({limit: 10, raw: true});
+    let response =  results.map(result => _.omit(result,["createdAt","updatedAt"]));
+
+    return res.status(200).json(response);
+};
+
+exports.get_series_by_id = async (req, res) => {
+
+    if(validator.isInt(req.params.id)){
+        let series_db = models.series;
+        let result = await series_db.findOne({where: { id: req.params.id }, raw: true});
+        let response = _.omit(result,["createdAt","updatedAt"]);
+        return res.status(200).json(response);
+    } else {
+        return res.status(400).json({message: "Invalid Request"});
+    }
+
+};
