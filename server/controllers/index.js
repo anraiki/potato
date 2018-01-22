89c4ed532cea667b09bf9fe726fb9aee8af49e68
Anri <anri.tora@gmail.com>
2018-01-21 19:41:12 -0800

    Prepend API Endpoints with API Versioning

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -6,21 +6,24 @@ let express          = require('express'),
     users            = require("./users.js"),
     router           = express.Router();
 
-//Authentication Block
-router.post ('/api/register/'           , authentication.register_user);
-router.post ('/api/login/'              , authentication.login_user);
-
+//Versioning
+router.use('/api/v1',router);
+    
 //Middleware
 //-Token Check for Creating, Deleting, and Updating.
 //-Read is Free!
 
+//Authentication Block
+router.post ('/register/'           , authentication.register_user);
+router.post ('/login/'              , authentication.login_user);
+
 //Series Block
-router.get  ('/api/series/'             , series.get_list_of_series);
-router.get  ('/api/series/:id'          , series.get_series_by_id);
+router.get  ('/series/'             , series.get_list_of_series);
+router.get  ('/series/:id'          , series.get_series_by_id);
 
-router.get  ('/api/user/:id'            , users.get_users_info_by_id);
+router.get  ('/user/:id'            , users.get_users_info_by_id);
 
-router.get  ('/api/scanlators/'         , authentication.login_user);
-router.get  ('/api/scanlators/:id'      , authentication.login_user);
+router.get  ('/scanlators/'         , authentication.login_user);
+router.get  ('/scanlators/:id'      , authentication.login_user);
 
 module.exports = router;
